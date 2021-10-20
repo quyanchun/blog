5fd007cf1df224334316ac6a9639723196179d66
yanchun <quyanchun@mingxingtech.com>
2021-10-20 11:41:22 +0800

    first commit

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/yanchun-blog
+
+git pull
+
+ID=`ps -ef|grep node | grep vuepress |awk '{print $2}'`
+echo --- the process is $ID ---
+kill -9 $ID
+echo "killed $ID"
+
+#启动
+nohup npm run dev&
